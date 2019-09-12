5be1584dff879a6793e73ed2d639b5ae23593e49
busra <412469@student.fontys.nl>
2019-09-12 20:30:12 +0200

    PWA les 2 - offline caching

--- PWA/PWAles1/sample-site/service-worker.js
+++ PWA/PWAles1/sample-site/service-worker.js
@@ -1,5 +1,24 @@
+const cacheName="CACHE_v1";
+const appFiles=[
+    "manifest.json",
+    "js/scripts.js",
+    "css/stylesheet.css",
+    "index.html",
+    "images/cat.gif"
+];
+
+
+
+
 self.addEventListener("install",(installing)=>{
   console.log("Service Worker: I am being installed, hello world!");
+  installing.waitUntil(
+    caches.open(cacheName).then((cache)=>{
+      console.log("Service Worker: Caching important offline files");
+      return cache.addAll(appFiles);
+    })
+  );
+  
 });
 
 self.addEventListener("activate",(activating)=>{    
@@ -8,8 +27,29 @@ self.addEventListener("activate",(activating)=>{
 
 self.addEventListener("fetch",(fetching)=>{   
   console.log("Service Worker: User threw a ball, I need to fetch it!");
+
+  fetching.respondWith(
+    caches.match(fetching.request).then((response)=>{
+      console.log("Service Worker: Fetching resource "+fetching.request.url);
+      return response||fetch(fetching.request).then((response)=>{
+        console.log("Service Worker: Resource "+fetching.request.url+" not available in cache");
+        return caches.open(cacheName).then((cache)=>{
+            console.log("Service Worker: Caching (new) resource "+fetching.request.url);
+            cache.put(fetching.request,response.clone());
+          return response;
+        });
+      }).catch(function(){      
+        console.log("Service Worker: Fetching online failed, HAALLPPPP!!!");
+        //Do something else with the request (respond with a different cached file)
+      })
+    })
+  );
 });
 
 self.addEventListener("push",(pushing)=>{
     console.log("Service Worker: I received some push data, but because I am still very simple I don't know what to do with it :(");
 })
+
+//Put important offline files in cache on installation of the service worker
+
+
